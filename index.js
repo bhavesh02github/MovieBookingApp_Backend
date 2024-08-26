d8bfd2329c755b68dd0fbd57416c7ffebaabacfe
name <email>
2024-08-26 14:54:12 +0530

    day1

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 8000;
+const cookieParser = require('cookie-parser');
+
+
+require('dotenv').config();
+require('./db')
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Configure CORS with credentials
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true, // Allow credentials
+    })
+);
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'The API is working' });
+});
+
+
+app.use(bodyParser.json());
+const allowedOrigins = ['http://localhost:3000']; // Add more origins as needed
